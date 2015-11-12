1c14804f48c581343096cfa09300bb573ab2e40a
anusuyarc <anusuyarc89@gmail.com>
2015-11-12 00:35:25 -0800

    removing spaces

--- gdi-maps/js/meetup-map.js
+++ gdi-maps/js/meetup-map.js
@@ -5,16 +5,10 @@ var geocoder = L.mapbox.geocoder('mapbox.places');//
 
 var MEETUP_URL = "";
 
-
-
 function awesomeClick(){
     var htmlString = "";
-
     var meetup_key= document.getElementById('meetup-key').value;
-          // "d30645f3c726b341f2a6614d4f4b37";
     var meetup_event=document.getElementById('meetup-event').value;
-          // "Women-Who-Code-SF";
-
     var meetup_city = document.getElementById('city-geocode').value; //
 
     if(meetup_key=="" || meetup_event==""){
@@ -34,75 +28,68 @@ function awesomeClick(){
       }
     }
 
-
     $(document).ready(function(){
           
+      var MEETUP_URL = "http://api.meetup.com/2/events?key="+meetup_key+"&offset=0&format=json&limited_events=False&group_urlname="+meetup_event+"&sign=true&photo-host=public&page=20&fields=&order=time&desc=false&status=upcoming&callback=";
 
-          var MEETUP_URL = "http://api.meetup.com/2/events?key="+meetup_key+"&offset=0&format=json&limited_events=False&group_urlname="+meetup_event+"&sign=true&photo-host=public&page=20&fields=&order=time&desc=false&status=upcoming&callback=";
+      function convert(epoch){
+        var epoch;
+        var myDate = new Date(epoch);
+        return myDate.toLocaleString();
 
+       }; 
+
+        $.ajax({
+
+          url: MEETUP_URL,
+          type: "GET",
+          dataType: 'jsonp',
+          cache: false,
+          success: function(response){                      
+              // var htmlString = "";
         
+              $.each(response.results, function (i, item) {
 
+                  var mgeojson = L.mapbox.featureLayer({
+                    type: 'Feature',
+                    geometry: {
+                        type: 'Point',
+                        // coordinates here are in longitude, latitude order because
+                        // x, y is the standard for GeoJSON and many formats
+                        coordinates: [
+                          item.venue.lon,
+                          item.venue.lat
+                        ]
+                    },
+                    properties: {
+                        title: item.venue.name,
+                        description: item.venue.address_1,
+                        'marker-size': 'small',
+                        'marker-color': '#f95a61', /*#f95a61 #BE9A6B*/
+                        // 'marker-symbol': 'cafe',
+                    }
+                        
+                  })
 
-          function convert(epoch){
-              var epoch;
-              var myDate = new Date(epoch);
-              return myDate.toLocaleString();
+                  mgeojson.addTo(map);
+                  htmlString += '<h5><a href=\"www.google.com\"' + item.link + '" target="_blank">' + item.name + '</a></h5>';
+                  htmlString += '<p> Address: ' + item.venue.address_1 + '</br></p>' ;
+                  // htmlString += 'lon ' + item.venue.lon + ' lat ' + item.venue.lat + "<br/>";
+                  htmlString += '<p>'+ convert(item.time)+'</br></p>';
+                  // htmlString += '<button> Click this</button>'; 
+                  htmlString += '<hr>';
 
-           }; 
-          
-
-            $.ajax({
-
-              url: MEETUP_URL,
-              type: "GET",
-              dataType: 'jsonp',
-              cache: false,
-              success: function(response){                      
-                  // var htmlString = "";
-            
-                  $.each(response.results, function (i, item) {
-
-                      var mgeojson = L.mapbox.featureLayer({
-                        type: 'Feature',
-                        geometry: {
-                            type: 'Point',
-                            // coordinates here are in longitude, latitude order because
-                            // x, y is the standard for GeoJSON and many formats
-                            coordinates: [
-                              item.venue.lon,
-                              item.venue.lat
-                            ]
-                        },
-                        properties: {
-                            title: item.venue.name,
-                            description: item.venue.address_1,
-                            'marker-size': 'small',
-                            'marker-color': '#f95a61', /*#f95a61 #BE9A6B*/
-                            // 'marker-symbol': 'cafe',
-                        }
-                            
-                      })
-
-                      mgeojson.addTo(map);
-                      htmlString += '<h5><a href=\"www.google.com\"' + item.link + '" target="_blank">' + item.name + '</a></h5>';
-                      htmlString += '<p> Address: ' + item.venue.address_1 + '</br></p>' ;
-                      // htmlString += 'lon ' + item.venue.lon + ' lat ' + item.venue.lat + "<br/>";
-                      htmlString += '<p>'+ convert(item.time)+'</br></p>';
-                      // htmlString += '<button> Click this</button>'; 
-                      htmlString += '<hr>';
-
-                      $("ol").append(htmlString);
-                      htmlString="";
-                      
-                      // console.log(response.results, i, item );
-                  }); /* end of .each item*/
-              } /*end of sucess response*/
-     
-            }); /*ajax request ends */
+                  $("ol").append(htmlString);
+                  htmlString="";
+                  
+                  // console.log(response.results, i, item );
+              }); /* end of .each item*/
+          } /*end of sucess response*/
+ 
+        }); /*ajax request ends */
 
     });
-
-};
+}
 
 
 
